f4195435113d183986d64718b4f163a6fb71914e
evenilson <evenilsonguerreiro@gmail.com>
2023-12-13 22:22:33 -0300

    seçao faq rodapé

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
+    const questions = document.querySelectorAll('[data-faq-question]')
 
 
     for(let i = 0; i < buttons.length; i++){
@@ -11,14 +12,30 @@ document.addEventListener('DOMContentLoaded',function(){
             removerBotaoactive()
             botao.target.classList.add('shows__tabs__button--is-active')
         })
+
     }
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click',abreOufecharesposta)
+    }
+   
 })
+
+function abreOufecharesposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+
+}
 
 function removerBotaoactive(){
     const buttons = document.querySelectorAll('[data-tab-button]')
 
     for(let i = 0; i < buttons.length; i++){
         buttons[i].classList.remove('shows__tabs__button--is-active')
+       
+        
     }
 
 }
