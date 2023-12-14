6b8488d45c172763ec731dd3a0ffa22cc3f58fdd
evenilson <evenilsonguerreiro@gmail.com>
2023-12-14 16:12:02 -0300

    final

--- src/styles/main.js
+++ src/styles/main.js
@@ -2,25 +2,59 @@ document.addEventListener('DOMContentLoaded',function(){
     const buttons = document.querySelectorAll('[data-tab-button]')
     const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
-    for(let i = 0; i < buttons.length; i++){
-        buttons[i].addEventListener('click', function(botao){
-            const abaAlvo = botao.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
-            escondAbas();
-            aba.classList.add('shows__list--is-active')
-            removerBotaoactive()
-            botao.target.classList.add('shows__tabs__button--is-active')
+    window.addEventListener('scroll',function(){
+            const possicaoAtual = window.scrollY
+
+            if(possicaoAtual < alturaHero){
+                ocutarElementos();
+            } else {
+                exibirElementos();
+            }
         })
+        
 
-    }
 
-    for(let i = 0; i < questions.length; i++){
-        questions[i].addEventListener('click',abreOufecharesposta)
-    }
+
+// programação das abas //
+        for(let i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener('click', function(botao){
+                const abaAlvo = botao.target.dataset.tabButton;
+                const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
+                escondAbas();
+                aba.classList.add('shows__list--is-active')
+                removerBotaoactive()
+                botao.target.classList.add('shows__tabs__button--is-active')
+            })
+    
+        }
+        for(let i = 0; i < questions.length; i++){
+            questions[i].addEventListener('click',abreOufecharesposta)
+        }
    
-})
+    
 
+    })
+ 
+function ocutarElementos(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibirElementos(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+
+
+
+
+
+
+// programação das acordions //
 function abreOufecharesposta(elemento){
     const classe = 'faq__questions__item--is-open';
     const elementoPai = elemento.target.parentNode;
@@ -37,7 +71,6 @@ function removerBotaoactive(){
        
         
     }
-
 }
 
 
